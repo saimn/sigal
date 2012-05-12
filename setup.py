588645ff5f4eb776c0fb6b80bb09f44719ba5786
Simon <contact@saimon.org>
2012-05-13 00:55:47 +0200

    update setup.py, MANIFEST, LICENSE and gitignore

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python2
 # -*- coding:utf-8 -*-
 
-from distutils.core import setup
+from setuptools import setup
 import sys
 
 requires = ['imaging', 'jinja2', 'docutils']
@@ -12,20 +12,13 @@ setup(
     name = 'sigal',
     version = '0.1',
     description = 'simple static gallery generator',
-    long_description =
-    '''
-    sigal is yet another python script to prepare a static gallery of images:
-
-    * resize images, create thumbnails with some options (squared thumbs, ...).
-    * generate html pages.
-    ''',
+    long_description = open('README').read(),
     url = 'https://github.com/saimn/sigal',
     author = 'Simon C.',
     author_email = 'contact@saimon.org',
     license = 'GPLv3',
-    requires = requires,
-    # install_requires  =  requires,
+    include_package_data = True,
+    install_requires  =  requires,
     packages = ['sigal'],
     scripts = ['bin/sigal'],
     )
-
