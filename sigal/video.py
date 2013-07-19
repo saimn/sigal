1f40adc56178d6b22106502744bb599b253a71d5
Christophe-Marie Duquesne <chm.duquesne@gmail.com>
2013-07-19 12:49:37 +0200

    Don't transcode if unnecessary + don't enlarge

--- sigal/video.py
+++ sigal/video.py
@@ -23,7 +23,24 @@
 from __future__ import with_statement
 import subprocess
 import os
+import re
+import shutil
 import sigal.image
+
+def vid_size(source):
+    """Returns the dimensions of the video"""
+    pattern = re.compile(r'Stream.*Video.* ([0-9]+)x([0-9]+)')
+    p = subprocess.Popen(['ffmpeg', '-i', source],
+                         stdout=subprocess.PIPE,
+                         stderr=subprocess.PIPE)
+    stdout, stderr = p.communicate()
+    match = pattern.search(stderr)
+    if match:
+        x, y = int(match.groups()[0]), int(match.groups()[1])
+    else:
+        x = y = 0
+    return (x, y)
+
 
 def generate_video(source, outname, size, options={}):
     """Video processor
@@ -33,22 +50,39 @@ def generate_video(source, outname, size, options={}):
     :param options: array of options passed to ffmpeg
 
     """
-    # 2013-07-16: Until we find a better solution, we don't keep the h
-    # param: We want to preserve the ratio of the original file
-    # (resizeToFit is not available in ffmpeg) see
+    # Don't transcode if source is in the required format and
+    # has fitting datedimensions, copy instead.
+    w_src, h_src = vid_size(source)
+    w_dst, h_dst = size
+    base, src_ext = os.path.splitext(source)
+    base, dst_ext = os.path.splitext(outname)
+    if dst_ext == src_ext and w_src <= w_dst and h_src <= w_dst:
+        shutil.copy(source, outname)
+        return
+
     # http://stackoverflow.com/questions/8218363/maintaining-ffmpeg-aspect-ratio
-    #
-    # 2013-07-17: encoding options improved, thanks to
+    # + I made a drawing on paper to figure this out
+    if h_dst * w_src < h_src * w_dst:
+        # biggest fitting dimension is height
+        resize_opt = ['-vf', "scale=trunc(oh*a/2)*2:%i" % h_dst]
+    else:
+        # biggest fitting dimension is width
+        resize_opt = ['-vf', "scale=%i:trunc(ow/a/2)*2" % w_dst]
+
+    # do not resize if input dimensions are smaller than output dimensions
+    if w_src <= w_dst and h_src <= h_dst:
+        resize_opt = []
+
+    # Encoding options improved, thanks to
     # http://ffmpeg.org/trac/ffmpeg/wiki/vpxEncodingGuide
-    (w, h) = size
     with open("/dev/null") as devnull:
         subprocess.call(['ffmpeg', '-i', source, '-y',
             '-crf', options.get('crf', '10'),
             '-b:v', options.get('bitrate', '1.6M'),
             '-qmin', options.get('qmin', '4'),
-            '-qmax',options.get('qmax', '63'),
-            '-vf', "scale=%i:trunc(ow/a/2)*2'" % w,
-            outname], stderr=devnull)
+            '-qmax', options.get('qmax', '63')] +
+            resize_opt + [outname],
+            stderr=devnull)
 
 def generate_thumbnail(source, outname, box, format, fit=True, options=None):
     "Create a thumbnail image"
