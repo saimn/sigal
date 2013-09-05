5ed5052b1f884c8aebe13e1567978b0915e331fa
Simon Conseil <contact@saimon.org>
2013-09-06 00:34:06 +0200

    Bump 0.5.0

--- sigal/pkgmeta.py
+++ sigal/pkgmeta.py
@@ -2,7 +2,7 @@
 
 __title__ = 'sigal'
 __author__ = u"Simon Conseil"
-__version__ = "0.4.1"
+__version__ = "0.5.0"
 __license__ = "MIT"
 __url__ = 'https://github.com/saimn/sigal'
 __all__ = ['__title__', '__author__', '__version__', '__license__', '__url__']
