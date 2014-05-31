05f71e86e38905d6c2b9012968e4e8fb0686d988
Simon Conseil <contact@saimon.org>
2014-06-01 00:13:57 +0200

    Add plugins in the test config file.

--- tests/sample/sigal.conf.py
+++ tests/sample/sigal.conf.py
@@ -8,7 +8,11 @@ keep_orig = True
 links = [('Example link', 'http://example.org'),
          ('Another link', 'http://example.org')]
 
-copyright = "An example copyright message"
+from sigal.plugins import copyright
+plugins = ['sigal.plugins.adjust', copyright]
+copyright = u"© An example copyright message"
+adjust_options = {'color': 0.0, 'brightness': 1.0,
+                  'contrast': 1.0, 'sharpness': 0.0}
 
 # theme = 'galleria'
 # thumb_size = (280, 210)
