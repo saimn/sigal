8986f5845493bfb6bdf8f5768f6611f1f1bd4aeb
Abdul Qabiz <abdul.qabiz@gmail.com>
2015-03-06 19:22:49 +0530

    Update code
    - Make watermark path relative to settings file
    - Document possible options for watermark_postion
    - Simplify code to use settings.get(<setting>, <default>)

--- sigal/plugins/watermark.py
+++ sigal/plugins/watermark.py
@@ -1,16 +1,40 @@
 # -*- coding: utf-8 -*-
 
+# Copyright (c) 2005 - Shane Hathaway (http://code.activestate.com/recipes/362879-watermark-with-pil/)
+# Copyright (c) 2015 - Abdul Qabiz
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to
+# deal in the Software without restriction, including without limitation the
+# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+# sell copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
+
 """Plugin which adds a watermark to the image.
 
 Settings:
 
 - ``watermark``: path to the watermark image.
-- ``watermark_position``: the watermark position (scale or tile)
-- ``watermark_opacity``: the watermark opacity (0.0 to 1.0)
+- ``watermark_position``: the watermark position either 'scale' or 'tile' or
+                          a 2-tuple giving the upper left corner, or
+                          a 4-tuple defining the left, upper, right, and lower pixel coordinate, or
+                          `None` (same as (0, 0)).
+                          If a 4-tuple is given, the size of the pasted image must match the size of the region.
+- ``watermark_opacity``: the watermark opacity (0.0 to 1.0).
 
 """
 
-## Original code: http://code.activestate.com/recipes/362879-watermark-with-pil/
 
 import logging
 from PIL import ImageDraw, Image, ImageEnhance
@@ -31,6 +55,7 @@ def reduce_opacity(im, opacity):
     im.putalpha(alpha)
     return im
 
+
 def watermark(im, mark, position, opacity=1):
     """Adds a watermark to an image."""
     if opacity < 1:
@@ -39,7 +64,7 @@ def watermark(im, mark, position, opacity=1):
         im = im.convert('RGBA')
     # create a transparent layer the size of the image and draw the
     # watermark in that layer.
-    layer = Image.new('RGBA', im.size, (0,0,0,0))
+    layer = Image.new('RGBA', im.size, (0, 0, 0, 0))
     if position == 'tile':
         for y in range(0, im.size[1], mark.size[1]):
             for x in range(0, im.size[0], mark.size[0]):
@@ -57,16 +82,12 @@ def watermark(im, mark, position, opacity=1):
     # composite the watermark with the layer
     return Image.composite(layer, im, layer)
 
+
 def add_watermark(img, settings=None):
     logger.debug('Adding watermark to %r', img)
     mark = Image.open(settings['watermark'])
-    position = 'scale'
-    opacity = 1
-    if settings.get('watermark_position'):
-        position = settings['watermark_position']
-    if settings.get('watermark_opacity'):
-        opacity = settings["watermark_opacity"]
-
+    position = settings.get('watermark_position', 'scale')
+    opacity = settings.get("watermark_opacity", 1)
     return watermark(img, mark, position, opacity)
 
 
