94c98588b8d0009cf11e460c85c9a96a51104cb2
Simon Conseil <contact@saimon.org>
2015-04-20 00:05:07 +0200

    Don't share the settings fixture between tests.

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,7 +8,7 @@ from sigal.settings import read_settings
 CURRENT_DIR = os.path.abspath(os.path.dirname(__file__))
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture
 def settings():
     """Read the sample config file."""
     return read_settings(os.path.join(CURRENT_DIR, 'sample', 'sigal.conf.py'))
