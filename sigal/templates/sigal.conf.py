3838533d85bcb354a46a6cc4579807e51ff3d87b
brent.bb <brent.bb+gh@gmail.com>
2018-02-04 22:37:25 -0500

    Match files_to_copy example text to match the other example

--- sigal/templates/sigal.conf.py
+++ sigal/templates/sigal.conf.py
@@ -224,9 +224,9 @@ ignore_files = []
 
 
 # Site Logo
-# Use a logo file in the side bar
+# Use a logo file in the sidebar
 # You must place the logo file into the theme's static images folder, which can be done using files_to_copy
-# files_to_copy = (('logo.png', 'static/images/logo.png'))
+# files_to_copy = (('extra/logo.png', 'static/images/logo.png'))
 # site_logo = 'logo.png'
 
 # --------
