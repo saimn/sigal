3afeba778d1ee3a5c4e1a8abe50a8ca496447615
brent.bb <brent.bb+gh@gmail.com>
2018-02-04 22:32:26 -0500

    Sidebar site logo image in Colorbox
    Replace the index title in the Colorbox sidebar with an optional logo.
    Use files_to_copy to place the logo file in the correct location.

--- sigal/templates/sigal.conf.py
+++ sigal/templates/sigal.conf.py
@@ -222,6 +222,13 @@ ignore_files = []
 # The default is 3 columns (176px).
 # colorbox_column_size = 3
 
+
+# Site Logo
+# Use a logo file in the side bar
+# You must place the logo file into the theme's static images folder, which can be done using files_to_copy
+# files_to_copy = (('logo.png', 'static/images/logo.png'))
+# site_logo = 'logo.png'
+
 # --------
 # Plugins
 # --------
