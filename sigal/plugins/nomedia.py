abac1b2914784cad07ff0a32ab00b38eec4066e3
Simon Conseil <contact@saimon.org>
2018-02-11 00:47:00 +0100

    Cleanup nomedia plugin

--- sigal/plugins/nomedia.py
+++ sigal/plugins/nomedia.py
@@ -56,10 +56,10 @@ from sigal import signals
 logger = logging.getLogger(__name__)
 
 
-def _remove_albums_with_subdirs(albums, keysToRemove, prefix=""):
-    for keyToRemove in keysToRemove:
+def _remove_albums_with_subdirs(albums, keystoremove, prefix=""):
+    for keytoremove in keystoremove:
         for key in list(albums.keys()):
-            if key.startswith(prefix + keyToRemove):
+            if key.startswith(prefix + keytoremove):
                 # subdirs' target directories have already been created,
                 # remove them first
                 try:
@@ -84,36 +84,43 @@ def _remove_albums_with_subdirs(albums, keysToRemove, prefix=""):
 
 def filter_nomedia(album, settings=None):
     """Removes all filtered Media and subdirs from an Album"""
-    nomediaPath = os.path.join(album.src_path, ".nomedia")
+    nomediapath = os.path.join(album.src_path, ".nomedia")
 
-    if os.path.isfile(nomediaPath):
-        if os.path.getsize(nomediaPath) == 0:
-            logger.info("Ignoring album '%s' because of present 0-byte .nomedia file", album.name)
+    if os.path.isfile(nomediapath):
+        if os.path.getsize(nomediapath) == 0:
+            logger.info("Ignoring album '%s' because of present 0-byte "
+                        ".nomedia file", album.name)
 
-            # subdirs have been added to the gallery already, remove them there, too
+            # subdirs have been added to the gallery already, remove them
+            # there, too
             _remove_albums_with_subdirs(album.gallery.albums, [album.path])
             try:
                 os.rmdir(album.dst_path)
             except OSError as e:
-                # directory was created and populated with images in a previous run => keep it
+                # directory was created and populated with images in a
+                # previous run => keep it
                 pass
 
-            # cannot set albums => empty subdirs so that no albums are generated
+            # cannot set albums => empty subdirs so that no albums are
+            # generated
             album.subdirs = []
             album.medias = []
 
         else:
-            with io.open(nomediaPath, "r") as nomediaFile:
-                logger.info("Found a .nomedia file in %s, ignoring its entries", album.name)
-                ignoredEntries = nomediaFile.read().split("\n")
+            with io.open(nomediapath, "r") as nomediaFile:
+                logger.info("Found a .nomedia file in %s, ignoring its "
+                            "entries", album.name)
+                ignored = nomediaFile.read().split("\n")
 
-                album.medias = list(filter(lambda media: media.src_filename not in ignoredEntries,
-                                           album.medias))
-                album.subdirs = list(filter(lambda dirName: dirName not in ignoredEntries,
-                                            album.subdirs))
+                album.medias = [media for media in album.medias
+                                if media.src_filename not in ignored]
+                album.subdirs = [dirname for dirname in album.subdirs
+                                 if dirname not in ignored]
 
-                # subdirs have been added to the gallery already, remove them there, too
-                _remove_albums_with_subdirs(album.gallery.albums, ignoredEntries, album.path + os.path.sep)
+                # subdirs have been added to the gallery already, remove
+                # them there, too
+                _remove_albums_with_subdirs(album.gallery.albums,
+                                            ignored, album.path + os.path.sep)
 
 
 def register(settings):
