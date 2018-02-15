saimn/sigal

commit 6c5c4edd138fe1d46bbddeec78fd4d3426cc324e

Update galleria v1.5.7

sigal/themes/galleria/static/themes/classic/galleria.classic.js
@@ -1,0 +1,102 @@
+/**
+ * Galleria Classic Theme 2017-02-13
+ * http://galleria.io
+ *
+ * Copyright (c) 2010 - 2017 worse is better UG
+ * Licensed under the MIT license
+ * https://raw.github.com/worseisbetter/galleria/master/LICENSE
+ *
+ */
+
+(function($) {
+
+/*global window, jQuery, Galleria */
+
+Galleria.addTheme({
+    name: 'classic',
+    version: 1.5,
+    author: 'Galleria',
+    css: 'galleria.classic.css',
+    defaults: {
+        transition: 'slide',
+        thumbCrop:  'height',
+
+        // set this to false if you want to show the caption all the time:
+        _toggleInfo: true
+    },
+    init: function(options) {
+
+        Galleria.requires(1.4, 'This version of Classic theme requires Galleria 1.4 or later');
+
+        // add some elements
+        this.addElement('info-link','info-close');
+        this.append({
+            'info' : ['info-link','info-close']
+        });
+
+        // cache some stuff
+        var info = this.$('info-link,info-close,info-text'),
+            touch = Galleria.TOUCH;
+
+        // show loader & counter with opacity
+        this.$('loader,counter').show().css('opacity', 0.4);
+
+        // some stuff for non-touch browsers
+        if (! touch ) {
+            this.addIdleState( this.get('image-nav-left'), { left:-50 });
+            this.addIdleState( this.get('image-nav-right'), { right:-50 });
+            this.addIdleState( this.get('counter'), { opacity:0 });
+        }
+
+        // toggle info
+        if ( options._toggleInfo === true ) {
+            info.bind( 'click:fast', function() {
+                info.toggle();
+            });
+        } else {
+            info.show();
+            this.$('info-link, info-close').hide();
+        }
+
+        // bind some stuff
+        this.bind('thumbnail', function(e) {
+
+            if (! touch ) {
+                // fade thumbnails
+                $(e.thumbTarget).css('opacity', 0.6).parent().hover(function() {
+                    $(this).not('.active').children().stop().fadeTo(100, 1);
+                }, function() {
+                    $(this).not('.active').children().stop().fadeTo(400, 0.6);
+                });
+
+                if ( e.index === this.getIndex() ) {
+                    $(e.thumbTarget).css('opacity',1);
+                }
+            } else {
+                $(e.thumbTarget).css('opacity', this.getIndex() ? 1 : 0.6).bind('click:fast', function() {
+                    $(this).css( 'opacity', 1 ).parent().siblings().children().css('opacity', 0.6);
+                });
+            }
+        });
+
+        var activate = function(e) {
+            $(e.thumbTarget).css('opacity',1).parent().siblings().children().css('opacity', 0.6);
+        };
+
+        this.bind('loadstart', function(e) {
+            if (!e.cached) {
+                this.$('loader').show().fadeTo(200, 0.4);
+            }
+            window.setTimeout(function() {
+                activate(e);
+            }, touch ? 300 : 0);
+            this.$('info').toggle( this.hasInfo() );
+        });
+
+        this.bind('loadfinish', function(e) {
+            this.$('loader').fadeOut(200);
+        });
+    }
+});
+
+}(jQuery));

Makefile
@@ -1,7 +1,6 @@
 COLORBOX_PATH=sigal/themes/colorbox/static/css
-GALLERIA_PATH=sigal/themes/galleria/static/css
 
-all: colorbox galleria
+all: colorbox
 
 init:
 	pip install -r requirements.txt
@@ -12,18 +11,15 @@ docs:
 colorbox:
 	cat $(COLORBOX_PATH)/{base,skeleton,colorbox,style}.css | cssmin > $(COLORBOX_PATH)/style.min.css
 
-galleria:
-	cat $(GALLERIA_PATH)/{normalize,style}.css | cssmin > $(GALLERIA_PATH)/style.min.css
-
 test:
 	py.test
 
 coverage:
 	py.test --cov sigal --cov-report term --cov-report=html
 
-publish: colorbox galleria
+publish: colorbox
 	python setup.py register
 	python setup.py sdist bdist_wheel
 	twine upload dist/*
 
-.PHONY: colorbox galleria docs
+.PHONY: colorbox  docs

sigal/themes/galleria/static/classic/galleria.classic.css
@@ -1,222 +1,0 @@
-/* Galleria Classic Theme 2012-08-07 | https://raw.github.com/aino/galleria/master/LICENSE | (c) Aino */
-
-#galleria-loader{height:1px!important}
-
-.galleria-container {
-  position: relative;
-  overflow: hidden;
-}
-.galleria-container img {
-  -moz-user-select: none;
-  -webkit-user-select: none;
-  -o-user-select: none;
-}
-.galleria-stage {
-  background: #222;
-  background: rgba( 0, 0, 0, .15 );
-  position: absolute;
-  top: 0;
-  bottom: 80px;
-  left: 10px;
-  right: 10px;
-  overflow:hidden;
-  -moz-border-radius: 4px;
-  -webkit-border-radius: 4px;
-  border-radius: 4px;
-}
-.galleria-thumbnails-container {
-  height: 70px;
-  bottom: 0;
-  position: absolute;
-  left: 10px;
-  right: 10px;
-  z-index: 2;
-}
-.galleria-carousel .galleria-thumbnails-list {
-  margin-left: 30px;
-  margin-right: 30px;
-}
-.galleria-thumbnails .galleria-image {
-  height: 60px;
-  width: 90px;
-  background: transparent;
-  margin: 0 10px 0 0;
-  float: left;
-  cursor: pointer;
-}
-.galleria-counter {
-  position: absolute;
-  bottom: 10px;
-  left: 10px;
-  text-align: right;
-  z-index: 2;
-}
-.galleria-loader {
-  width: 20px;
-  height: 20px;
-  position: absolute;
-  top: 10px;
-  right: 10px;
-  z-index: 2;
-  display: none;
-  background: url('classic-loader.gif') no-repeat 2px 2px;
-}
-.galleria-info {
-  width: 50%;
-  top: 15px;
-  left: 25px;
-  z-index: 2;
-  position: absolute;
-}
-.galleria-info-text {
-  background: #000;
-  background: rgba(0, 0, 0, .4);
-  padding: 15px;
-  display: none;
-  -moz-border-radius: 4px;
-  -webkit-border-radius: 4px;
-  border-radius: 4px;
-}
-.galleria-info-title {
-  font-size: 1.4em;
-  line-height: 1.4em;
-  font-weight: bold;
-  margin: 0;
-  color: #fff;
-}
-.galleria-info-description {
-  font-size: 1.2em;
-  line-height: 1.2em;
-  margin: 0;
-  color: #bbb;
-}
-.galleria-info-title+.galleria-info-description { margin-top: 7px; }
-.galleria-info-close {
-  width: 9px;
-  height: 9px;
-  position: absolute;
-  top: 10px;
-  right: 10px;
-  background-position: -753px -11px;
-  opacity: .5;
-  filter: alpha(opacity=50);
-  cursor: pointer;
-  display: none;
-}
-.notouch .galleria-info-close:hover{
-  opacity:1;
-  filter: alpha(opacity=100);
-}
-.touch .galleria-info-close:active{
-  opacity:1;
-  filter: alpha(opacity=100);
-}
-.galleria-info-link {
-  background-position: -669px -5px;
-  opacity: .7;
-  filter: alpha(opacity=70);
-  position: absolute;
-  width: 20px;
-  height: 20px;
-  cursor: pointer;
-}
-.notouch .galleria-info-link:hover {
-  opacity: 1;
-  filter: alpha(opacity=100);
-}
-.touch .galleria-info-link:active {
-  opacity: 1;
-  filter: alpha(opacity=100);
-}
-.galleria-image-nav {
-  position: absolute;
-  top: 50%;
-  margin-top: -62px;
-  width: 100%;
-  height: 62px;
-  left: 0;
-}
-.galleria-image-nav-left,
-.galleria-image-nav-right {
-  opacity: .3;
-  filter: alpha(opacity=30);
-  cursor: pointer;
-  width: 62px;
-  height: 124px;
-  position: absolute;
-  left: 15px;
-  z-index: 2;
-  background-position: 0 46px;
-}
-.galleria-image-nav-right {
-  left: auto;
-  right: 15px;
-  background-position: -254px 46px;
-  z-index: 2;
-}
-.notouch .galleria-image-nav-left:hover,
-.notouch .galleria-image-nav-right:hover {
-  opacity: 1;
-  filter: alpha(opacity=100);
-}
-.touch .galleria-image-nav-left:active,
-.touch .galleria-image-nav-right:active {
-  opacity: 1;
-  filter: alpha(opacity=100);
-}
-.galleria-thumb-nav-left,
-.galleria-thumb-nav-right {
-  cursor: pointer;
-  display: none;
-  background-position: -495px 5px;
-  position: absolute;
-  left: 0;
-  top: 0;
-  height: 40px;
-  width: 23px;
-  z-index: 3;
-  opacity: .8;
-  filter: alpha(opacity=80);
-}
-.galleria-thumb-nav-right {
-  background-position: -578px 5px;
-  border-right: none;
-  right: 0;
-  left: auto;
-}
-.galleria-thumbnails-container .disabled {
-  opacity: .2;
-  filter: alpha(opacity=20);
-  cursor: default;
-}
-.notouch .galleria-thumb-nav-left:hover,
-.notouch .galleria-thumb-nav-right:hover {
-  opacity: 1;
-  filter: alpha(opacity=100);
-  background-color: #111;
-}
-.touch .galleria-thumb-nav-left:active,
-.touch .galleria-thumb-nav-right:active {
-  opacity: 1;
-  filter: alpha(opacity=100);
-  background-color: #111;
-}
-.notouch .galleria-thumbnails-container .disabled:hover {
-  opacity: .2;
-  filter: alpha(opacity=20);
-  background-color: transparent;
-}
-
-.galleria-carousel .galleria-thumb-nav-left,
-.galleria-carousel .galleria-thumb-nav-right {
-  display: block;
-}
-.galleria-thumb-nav-left,
-.galleria-thumb-nav-right,
-.galleria-info-link,
-.galleria-info-close,
-.galleria-image-nav-left,
-.galleria-image-nav-right {
-  background-image: url('classic-map.png');
-  background-repeat: no-repeat;
-}

sigal/themes/galleria/static/css/style.min.css
@@ -1,1 +1,0 @@
-/*!normalize.css v3.0.1 | MIT License | git.io/normalize */ html{font-family:sans-serif;-ms-text-size-adjust:100%;-webkit-text-size-adjust:100%}body{margin:0}article,aside,details,figcaption,figure,footer,header,hgroup,main,nav,section,summary{display:block}audio,canvas,progress,video{display:inline-block;vertical-align:baseline}audio:not([controls]){display:none;height:0}[hidden],template{display:none}a{background:transparent}a:active,a:hover{outline:0}abbr[title]{border-bottom:1px dotted}b,strong{font-weight:bold}dfn{font-style:italic}h1{font-size:2em;margin:.67em 0}mark{background:#ff0;color:#000}small{font-size:80%}sub,sup{font-size:75%;line-height:0;position:relative;vertical-align:baseline}sup{top:-0.5em}sub{bottom:-0.25em}img{border:0}svg:not(:root){overflow:hidden}figure{margin:1em 40px}hr{-moz-box-sizing:content-box;box-sizing:content-box;height:0}pre{overflow:auto}code,kbd,pre,samp{font-family:monospace,monospace;font-size:1em}button,input,optgroup,select,textarea{color:inherit;font:inherit;margin:0}button{overflow:visible}button,select{text-transform:none}button,html input[type="button"],input[type="reset"],input[type="submit"]{-webkit-appearance:button;cursor:pointer}button[disabled],html input[disabled]{cursor:default}button::-moz-focus-inner,input::-moz-focus-inner{border:0;padding:0}input{line-height:normal}input[type="checkbox"],input[type="radio"]{box-sizing:border-box;padding:0}input[type="number"]::-webkit-inner-spin-button,input[type="number"]::-webkit-outer-spin-button{height:auto}input[type="search"]{-webkit-appearance:textfield;-moz-box-sizing:content-box;-webkit-box-sizing:content-box;box-sizing:content-box}input[type="search"]::-webkit-search-cancel-button,input[type="search"]::-webkit-search-decoration{-webkit-appearance:none}fieldset{border:1px solid #c0c0c0;margin:0 2px;padding:.35em .625em .75em}legend{border:0;padding:0}textarea{overflow:auto}optgroup{font-weight:bold}table{border-collapse:collapse;border-spacing:0}td,th{padding:0}body{background-color:#242424;color:#aaa;text-shadow:0 1px 3px rgba(0,0,0,0.7)}.container{margin:0 auto;text-align:left;width:96%}a,a:link,a:visited{color:#999;text-decoration:underline}a:hover{color:#dadada}abbr[title]{text-decoration:none;font-variant:none;border-bottom:none}abbr[title="exposure"]::after{content:"s"}abbr[title="film speed"]::before{content:"ISO "}abbr[title="aperture"]::before{content:"f/"}abbr[title="focal length"]::after{content:"mm"}abbr[title="camera make and model"],abbr[title="date"]{display:block}header{padding:1em 0;color:#eee}header h1,header h2{color:#eee;margin:10px 0}header h1 a,header h1 a:link,header h1 a:visited,header h2 a,header h2 a:link,header h2 a:visited{color:#eee;text-shadow:0 2px 0 #000;text-decoration:none}header h1 a:hover,header h2 a:hover{color:#fff;text-decoration:none}#menu ul{list-style-type:none;margin:0 0 10px;padding:0}#menu ul li{display:inline-block}#menu ul li a,#menu ul li a:link,#menu ul li a:visited{color:#aaa;border-bottom:1px solid #aaa;padding-bottom:2px;margin-left:5px;text-decoration:none}#menu ul li a:hover{color:#eee;border-color:#eee;text-decoration:none}#albums ul{list-style-type:none;padding-left:0}#albums ul li{display:inline-block;margin:0 55px 30px 0;text-align:center;vertical-align:top;width:280px}#albums ul li:nth-child(3n+3){margin-right:0}#albums ul li a img{opacity:1;-webkit-transition:opacity .2s ease-in;-moz-transition-property:opacity;-moz-transition-duration:.2s;-moz-box-shadow:0 1px 6px rgba(0,0,0,0.6);-webkit-box-shadow:0 1px 6px rgba(0,0,0,0.6);-o-box-shadow:0 1px 6px rgba(0,0,0,0.6);box-shadow:0 1px 6px rgba(0,0,0,0.6)}#albums ul li a:hover img{opacity:.5}.album_title{display:block;color:#eee;font-size:1.3em;font-variant:small-caps;font-weight:bold}#gallery{line-height:0;width:100%;height:600px;clear:both}#gallery video{position:absolute;top:10%;width:100%;margin:0 auto}.icons{top:10px;right:20px;padding:5px;z-index:2;position:absolute;text-align:right}.icons a{cursor:pointer}.icons img{opacity:.7}.icons img:hover{opacity:.9}#galleria-map{position:absolute;bottom:80px;left:10px;top:0;right:10px}footer{clear:both;display:block;margin:1em 0;text-align:center}footer a:link,footer a:visited{font-weight:bold;text-decoration:none}footer a:hover{border-bottom:1px solid;text-decoration:none}@media only screen and (min-width:980px){.container{width:960px}#gallery{width:980px;margin:0 0 40px -10px}header h1,#menu{display:inline-block;width:49.5%}#menu{text-align:right}}

sigal/themes/galleria/static/plugins/history/galleria.history.js
@@ -1,0 +1,147 @@
+/**
+ * Galleria History Plugin 2016-09-03
+ * http://galleria.io
+ *
+ * Copyright (c) 2010 - 2017 worse is better UG
+ * Licensed under the MIT license
+ * https://raw.github.com/worseisbetter/galleria/master/LICENSE
+ *
+ */
+
+(function( $, window ) {
+
+/*global jQuery, Galleria, window */
+
+Galleria.requires(1.25, 'The History Plugin requires Galleria version 1.2.5 or later.');
+
+Galleria.History = (function() {
+
+    var onloads = [],
+
+        init = false,
+
+        loc = window.location,
+
+        doc = window.document,
+
+        ie = Galleria.IE,
+
+        support = 'onhashchange' in window && ( doc.mode === undefined || doc.mode > 7 ),
+
+        iframe,
+
+        get = function( winloc ) {
+            if( iframe && !support && Galleria.IE ) {
+                winloc = winloc || iframe.location;
+            }  else {
+                winloc = loc;
+            }
+            return parseInt( winloc.hash.substr(2), 10 );
+        },
+
+        saved = get( loc ),
+
+        callbacks = [],
+
+        onchange = function() {
+            $.each( callbacks, function( i, fn ) {
+                fn.call( window, get() );
+            });
+        },
+
+        ready = function() {
+            $.each( onloads, function(i, fn) {
+                fn();
+            });
+
+            init = true;
+        },
+
+        setHash = function( val ) {
+            return '/' + val;
+        };
+
+    // always remove support if IE < 8
+    if ( support && ie < 8 ) {
+        support = false;
+    }
+
+    if ( !support ) {
+
+        $(function() {
+
+            var interval = window.setInterval(function() {
+
+                var hash = get();
+
+                if ( !isNaN( hash ) && hash != saved ) {
+                    saved = hash;
+                    loc.hash = setHash( hash );
+                    onchange();
+                }
+
+            }, 50);
+
+            if ( ie ) {
+
+                $('<iframe tabindex="-1" title="empty">').hide().attr( 'src', 'about:blank' ).one('load', function() {
+
+                    iframe = this.contentWindow;
+
+                    ready();
+
+                }).insertAfter(doc.body);
+
+            } else {
+                ready();
+            }
+        });
+    } else {
+        ready();
+    }
+
+    return {
+
+        change: function( fn ) {
+
+            callbacks.push( fn );
+
+            if( support ) {
+                window.onhashchange = onchange;
+            }
+        },
+
+        set: function( val ) {
+
+            if ( isNaN( val ) ) {
+                return;
+            }
+
+            if ( !support && ie ) {
+
+                this.ready(function() {
+
+                    var idoc = iframe.document;
+                    idoc.open();
+                    idoc.close();
+
+                    iframe.location.hash = setHash( val );
+
+                });
+            }
+
+            loc.hash = setHash( val );
+        },
+
+        ready: function(fn) {
+            if (!init) {
+                onloads.push(fn);
+            } else {
+                fn();
+            }
+        }
+    };
+}());
+
+}( jQuery, this ));
+

commit 6a18bb113230efe91cd6412501d699ba2f426062

Don't load both galleria.classic.css & minified
Fixes regression introduced in 6c5c4ed

sigal/themes/galleria/static/themes/classic/galleria.classic.js
@@ -16,7 +16,7 @@ Galleria.addTheme({
     name: 'classic',
     version: 1.5,
     author: 'Galleria',
-    css: 'galleria.classic.css',
+    css: 'galleria.classic.min.css',
     defaults: {
         transition: 'slide',
         thumbCrop:  'height',