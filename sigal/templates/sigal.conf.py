a368a6967298dfe4675d863dd36128b02f2662ed
Simon Conseil <contact@saimon.org>
2018-02-18 00:47:25 +0100

    Don't force logo image to be in images/

--- sigal/templates/sigal.conf.py
+++ sigal/templates/sigal.conf.py
@@ -222,11 +222,11 @@ ignore_files = []
 # The default is 3 columns (176px).
 # colorbox_column_size = 3
 
-
-# Site Logo
-# Use a logo file in the sidebar
-# You must place the logo file into the theme's static images folder, which can be done using files_to_copy
-# files_to_copy = (('extra/logo.png', 'static/images/logo.png'))
+# Site Logo - Use a logo file in the sidebar
+# Only for colorbox currently, it could be adapted for other themes
+# You must place the logo file into the theme's static images folder, which
+# can be done using 'files_to_copy':
+# files_to_copy = (('extra/logo.png', 'static/logo.png'))
 # site_logo = 'logo.png'
 
 # --------
