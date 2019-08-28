ee2b5cc5be606f9ebc417eb5bdaad7dee2e00fff
Simon Conseil <contact@saimon.org>
2019-08-29 00:08:20 +0200

    Fix typo in tests settings

--- tests/sample/sigal.conf.py
+++ tests/sample/sigal.conf.py
@@ -12,7 +12,7 @@ files_to_copy = (('../watermark.png', 'watermark.png'),)
 plugins = [
     'sigal.plugins.adjust',
     'sigal.plugins.copyright',
-    'sigal.plugins.extended_caching'
+    'sigal.plugins.extended_caching',
     'sigal.plugins.feeds',
     'sigal.plugins.nomedia',
     'sigal.plugins.watermark',
