1b27cc3258aed26821fa7ae9937b46e001f04682
Simon Conseil <contact@saimon.org>
2020-01-04 00:57:08 -0300

    Add testing for Python 3.8 and remove 3.5

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import sys
 from setuptools import setup
 
-if sys.version_info[:2] < (3, 5):
-    sys.exit('Sigal supports Python 3.5+ only')
+if sys.version_info[:2] < (3, 6):
+    sys.exit('Sigal supports Python 3.6+ only')
 
 setup(use_scm_version=True)
