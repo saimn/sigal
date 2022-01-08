saimn/sigal

commit ceab382e0252c4d7b25f8c2905b64628a9e15ba6

Replace deprecated imp usage

sigal/writer.py
@@ -20,7 +20,7 @@
 # FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 # IN THE SOFTWARE.
 
-import imp
+import importlib
 import logging
 import os
 import sys
@@ -80,7 +80,7 @@ class AbstractWriter:
         # handle optional filters.py
         filters_py = os.path.join(self.theme, 'filters.py')
         if os.path.exists(filters_py):
-            mod = imp.load_source('filters', filters_py)
+            mod = importlib.import_module(filters_py)
             for name in dir(mod):
                 if isinstance(getattr(mod, name), types.FunctionType):
                     env.filters[name] = getattr(mod, name)
@@ -88,8 +88,9 @@ class AbstractWriter:
         try:
             self.template = env.get_template(self.template_file)
         except TemplateNotFound:
-            self.logger.error('The template %s was not found in template folder %s.',
-                              self.template_file, theme_relpath)
+            self.logger.error(
+                'The template %s was not found in template folder %s.',
+                self.template_file, theme_relpath)
             sys.exit(1)
 
         # Copy the theme files in the output dir

commit 7aee5537fbe7f57f3271c486ef84451304694c13

fix loading filters.py using importlib
Regression from: https://github.com/saimn/sigal/commit/ceab382e0252c4d7b25f8c2905b64628a9e15ba6
Ref: https://docs.python.org/3/library/importlib.html#importing-a-source-file-directly

Fixes https://github.com/saimn/sigal/issues/451

sigal/writer.py
@@ -93,7 +93,10 @@ class AbstractWriter:
         # handle optional filters.py
         filters_py = os.path.join(self.theme, "filters.py")
         if os.path.exists(filters_py):
-            mod = importlib.import_module(filters_py)
+            module_spec = importlib.util.spec_from_file_location('filters', filters_py)
+            mod = importlib.util.module_from_spec(module_spec)
+            sys.modules['filters'] = mod
+            module_spec.loader.exec_module(mod)
             for name in dir(mod):
                 if isinstance(getattr(mod, name), types.FunctionType):
                     env.filters[name] = getattr(mod, name)