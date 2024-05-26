6210bf45a4479878448fd70f5ef7e947a9513021
Markus Blöchl <ususdei@googlemail.com>
2024-05-26 20:10:08 +0200

    Add support for EXIF data in .heic files

--- src/sigal/image.py
+++ src/sigal/image.py
@@ -225,7 +225,11 @@ def get_exif_data(filename):
 
     try:
         with warnings.catch_warnings(record=True) as caught_warnings:
-            exif = img._getexif() or {}
+            exif = {}
+            if hasattr(img, "_getexif") and callable(img._getexif):
+                exif = img._getexif()
+            elif "exif" in img.info:
+                exif = img.getexif()._get_merged_dict()
     except ZeroDivisionError:
         logger.warning("Failed to read EXIF data.")
         return None
@@ -295,7 +299,7 @@ def get_image_metadata(filename):
         logger.error("Could not open image %s metadata: %s", filename, e)
     else:
         try:
-            if os.path.splitext(filename)[1].lower() in (".jpg", ".jpeg"):
+            if os.path.splitext(filename)[1].lower() in (".jpg", ".jpeg", ".heic"):
                 exif = get_exif_data(img)
         except Exception as e:
             logger.warning("Could not read EXIF data from %s: %s", filename, e)
